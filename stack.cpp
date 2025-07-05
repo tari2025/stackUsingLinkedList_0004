774cc1c06886cff9ee416f901628501b6d2c4065
tari2025 <denilestari272@gmail.com>
2025-07-05 11:30:40 +0700

    switch choice case, case 1

--- stack.cpp
+++ stack.cpp
@@ -84,3 +84,11 @@ int main()
         cout << "Enter your choice: ";
         cin >> choice;
     }
+
+    switch (choice)
+    {
+    case 1:
+        cout << "Enter the value to push: ";
+        cin >> value;
+        stack.push(value);
+        break;
